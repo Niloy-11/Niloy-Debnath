839572f06b8bf145d72412354f10a64dad49abfa
Niloy Debnath <58338298+Niloy-11@users.noreply.github.com>
2021-06-14 00:00:10 +0600

    Update tictac.c

--- TicTacToe/tictac.c
+++ TicTacToe/tictac.c
@@ -11,8 +11,8 @@
 #define SIZE 3
 
 char tictac_board[SIZE][SIZE] = { {'1', '2', '3'},
-                           		  {'4', '5', '6'},
-                           		  {'7', '8', '9'} };
+                           	  {'4', '5', '6'},
+                         	  {'7', '8', '9'} };
 
 int row, col;
 void board_drawer();
